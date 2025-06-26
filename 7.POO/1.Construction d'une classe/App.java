413ab308692f033833d6ac5caa73d2b8b7b0996d
AIT LAHCEN Achraf <95300356+achraal@users.noreply.github.com>
2025-06-26 01:51:30 +0100

    Delete 7.POO directory

--- 7.POO/1.Construction d'une classe/App.java
+++
@@ -1,8 +0,0 @@
-public class App
-{
-    public static void main(String[] args)
-    {
-        Cat miau = new Cat();
-        Cat miaou = new Cat();
-    }
-}
--- 7.POO/2.Attributs/App.java
+++
@@ -1,8 +0,0 @@
-public class App
-{
-    public static void main(String[] args)
-    {
-        Cat miau = new Cat("achraf", 22);
-        Cat miaou = new Cat("Jason", 25);
-    }
-}
